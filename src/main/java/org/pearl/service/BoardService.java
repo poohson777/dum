e54fc7e4368532af4d1034c5488841b6da4c5460
JINJU <JINJU@JINJU>
2018-10-16 18:26:15 +0900

    ServiceTest 에러 고쳐야함~

---
+++ src/main/java/org/pearl/service/BoardService.java
@@ -0,0 +1,19 @@
+package org.pearl.service;
+
+import java.util.List;
+
+import org.pearl.domain.BoardVO;
+
+public interface BoardService {
+
+	public void register(BoardVO board);
+
+	public BoardVO get(Long bno);
+
+	public boolean modify(BoardVO board);
+
+	public boolean remove(Long bno);
+
+	public List<BoardVO> getList();
+
+}
---
+++ src/main/java/org/pearl/service/BoardServiceImpl.java
@@ -0,0 +1,54 @@
+package org.pearl.service;
+
+import java.util.List;
+
+import org.pearl.domain.BoardVO;
+import org.pearl.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@AllArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService {
+	
+	@Setter(onMethod = @__({ @Autowired }))
+	private BoardMapper mapper;
+
+	@Override
+	public void register(BoardVO board) {
+		
+		log.info("register...."+board);
+	    mapper.insertSelectKey(board);
+
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
