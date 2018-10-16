e7c34cc7cfcd17b713329a517d5cf2a936e504b9
JINJU <JINJU@JINJU>
2018-10-16 16:50:23 +0900

    CRUD Test Finish

--- src/main/java/org/pearl/mapper/BoardMapper.java
+++ src/main/java/org/pearl/mapper/BoardMapper.java
@@ -11,5 +11,11 @@ public interface BoardMapper {
 	public void insert(BoardVO board);
 	
 	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
 
 }
--- src/test/java/org/pearl/mapper/BoardMapperTests.java
+++ src/test/java/org/pearl/mapper/BoardMapperTests.java
@@ -22,16 +22,57 @@ public class BoardMapperTests {
 	public void getList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
-	
+
 	@Test
 	public void insertTest() {
 		BoardVO board = new BoardVO();
 		board.setTitle("친구를 만나느라");
 		board.setContent("샤샤샤");
 		board.setWriter("트와이수~~~~");
-		
+
 		mapper.insert(board);
-		
+
 		log.info(board);
+	}
+
+	@Test
+	public void insertSelectKeyTest() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("타이틀임돠 selectkey~");
+		board.setContent("내용임돠 SelectKey~");
+		board.setWriter("작성자임돠 SelectKey~");
+
+		mapper.insertSelectKey(board);
+
+		log.info(board);
+
+	}
+
+	@Test
+	public void readTest() {
+
+		BoardVO board = mapper.read(10L);
+
+		log.info(board);
+
+	}
+	
+	@Test
+	public void deleteTest() {
+		
+		log.info("deletCount~~~~~~" + mapper.delete(11L));
+	}
+	
+	@Test
+	public void updateTest() {
+	
+		BoardVO board = new BoardVO();
+		board.setBno(1L);
+		board.setContent("수정");
+		board.setTitle("수정타이들");
+		board.setWriter("수정글쓴이");
+		
+		mapper.update(board);
 	}
 }
